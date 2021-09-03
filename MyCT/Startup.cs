5e3749e1fe43dc17dfe5e6891e3d9ad830f14ca6
Abhihshek Pal <palabhishek322@gmail.com>
2021-09-03 14:46:17 +0530

    Star

--- MyCT/Startup.cs
+++ MyCT/Startup.cs
@@ -186,6 +186,8 @@ namespace MyCT
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+            app.UseCors(AllowedSpecificOrigins);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
